d84615a1b3ea16e8da995faa5b7cf56f3c2d0887
Subash Rijal <subash@metacloud.com.np>
2022-12-10 23:37:41 +0545

    remove unnessary codes

--- app/Console/Commands/GenerateCrud.php
+++ app/Console/Commands/GenerateCrud.php
@@ -57,89 +57,9 @@ class GenerateCrud extends Command
             $this->error("Fields are required to generate crud");
             return;
         };
-        // $this->serviceGenerator->generate($this->argument('model'));
-        // $this->controllerGenerator->generate($this->argument('model'));
-        // $this->migrationGenerator->generate($this->argument('model'), $this->option('fields'));
-        // $this->modelGenerator->generate($this->argument('model'), $this->option('fields'));
-    }
-
-
-
-    public function handleInterface()
-    {
-        $interfacePath = $this->getInterfaceSourceFilePath();
-        $this->makeDirectory(dirname($interfacePath));
-        $contents = $this->getControllerSourceFile();
-
-        if (!$this->files->exists($interfacePath)) {
-            $this->files->put($interfacePath, $contents);
-            $this->info("File : {$interfacePath} created");
-        } else {
-            $this->info("File : {$interfacePath} already exits");
-        }
-    }
-
-    protected $type = 'Services';
-
-    /**
-     * Get the stub file for the generator.
-     *
-     * @return string
-     */
-    protected function getStubPath($type)
-    {
-        return \base_path() . '/stubs/' . $type . '.stub';
-    }
-
-
-    /**
-     **
-     * Map the stub variables present in stub to its value
-     *
-     * @return array
-     *
-     */
-    public function getInterfaceStubVariables()
-    {
-        return [
-            'NAMESPACE' => 'App\\Contracts',
-            'CLASS_NAME' => $this->getSingularClassName($this->argument('model')),
-        ];
-    }
-
-    /**
-     * Get the stub path and the stub variables
-     *
-     * @return bool|mixed|string
-     *
-     */
-    public function getInterfaceSourceFile()
-    {
-        return $this->getStubContents($this->getStubPath('interface'), $this->getInterfaceStubVariables());
-    }
-
-    /**
-     * Get the full path of generate class
-     *
-     * @return string
-     */
-    public function getInterfaceSourceFilePath()
-    {
-        return base_path('app/Contracts') . '/' . $this->getSingularClassName($this->argument('model')) . 'ServiceInterface.php';
-    }
-
-    /**
-     * Build the directory for the class if necessary.
-     *
-     * @param  string  $path
-     * @return string
-     */
-    protected function makeDirectory($path)
-    {
-        if (!$this->files->isDirectory($path)) {
-            $this->files->makeDirectory($path, 0777, true, true);
-        }
-
-        return $path;
+        $this->serviceGenerator->generate($this->argument('model'));
+        $this->controllerGenerator->generate($this->argument('model'));
+        $this->migrationGenerator->generate($this->argument('model'), $this->option('fields'));
+        $this->modelGenerator->generate($this->argument('model'), $this->option('fields'));
     }
 }
