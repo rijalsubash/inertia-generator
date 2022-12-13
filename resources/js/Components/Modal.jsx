39a95ca891bd73d49b5f12e75d234749aa7c3f47
Subash Rijal <subash@metacloud.com.np>
2022-12-13 19:26:02 +0545

    basic crud completed

--- resources/js/Components/Modal.jsx
+++ resources/js/Components/Modal.jsx
@@ -77,12 +77,7 @@ export default function Modal({
                                 </button>
                             </header>
                             <div className="mt-4 mb-6">{children}</div>
-                            <footer className="flex flex-col items-center justify-end px-6 py-3 -mx-6 -mb-4 space-y-4 sm:space-y-0 sm:space-x-6 sm:flex-row bg-gray-50 dark:bg-gray-800">
 
-                                <button className="w-full px-5 py-3 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-lg sm:w-auto sm:px-4 sm:py-2 active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple">
-                                    Accept
-                                </button>
-                            </footer>
                         </div>
                     </Dialog.Panel>
                 </Transition.Child>
